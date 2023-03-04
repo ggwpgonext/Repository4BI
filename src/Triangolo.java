f3d971e1138d8bf36f18fb5cc249df380e3c1a2a
4b <4b@INF08>
2023-03-04 12:07:11 +0100

    IL MIO PRIMO PROGRAMMA :D !!! HELLO WORLD :D !!!
    PROGRAMMA TRIANGOLO
    
    04/03/2023
    
    DAJEROMA!!!!
    
    IL MIO PRIMO PROGRAMMA :D !!! HELLO WORLD :D !!!

---
+++ src/Triangolo.java
@@ -0,0 +1,36 @@
+
+public class Triangolo {
+	
+	private float base, altezza;
+
+	public Triangolo(float base, float altezza) {
+		this.base = base;
+		this.altezza = altezza;
+	}
+
+	public float getBase() {
+		return base;
+	}
+
+	public void setBase(float base) {
+		this.base = base;
+	}
+
+	public float getAltezza() {
+		return altezza;
+	}
+
+	public void setAltezza(float altezza) {
+		this.altezza = altezza;
+	}
+
+	
+	public String toString() {
+		return "Triangolo [base=" + base + ", altezza=" + altezza + "]";
+	}
+	
+	
+	
+	
+
+}
